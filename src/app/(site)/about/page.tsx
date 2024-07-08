b5727c01276279c5ddd6b971b77d4e6548f689dd
nocodedevos <nocode.devops@gmail.com>
2024-07-08 20:20:25 +0100

    Fotoprohub Landing Page

---
+++ src/app/(site)/about/page.tsx
@@ -0,0 +1,20 @@
+import About from "@/components/About";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Team from "@/components/Team";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us Page",
+};
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Breadcrumb pageName="About Us Page" />
+      <About />
+      <Team />
+    </main>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Fotoprohub</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Fotoprohub is a platform for photographers to showcase their work, connect with clients, and grow their business."/>
+      <link rel="icon" href="https://public-assets.fotoprohub.net/4.png" />
+    </>
+  );
+}
